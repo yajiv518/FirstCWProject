71281875dfdfd5ed951b10515ebc02721c2c5363
Vijay Prajapati <vijay.prajapati@aepl.com>
2017-06-27 18:38:43 +0530

    DAL updated

--- UsedCars/Interfaces/IDataAccess.cs
+++ UsedCars/Interfaces/IDataAccess.cs
@@ -10,8 +10,8 @@ namespace Interfaces
     public interface IDataAccess<T>
     {
         int Create(T a);
-        IEnumerable<T> GetAll(int limit, int offset, string order_by);
-        Stocks Read(int id);
+        //IEnumerable<T> GetAll(int limit, int offset, string order_by);
+        //Stocks Read(int id);
         int Delete(int id);
         int Edit(int id, T a);
     }
