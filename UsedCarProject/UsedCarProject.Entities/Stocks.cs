f78d3fa355b286b0a34528bfdd0758fe7ba73191
Snehal Niranjan Dange <snehal.dange@aepl.com>
2017-06-27 14:41:27 +0530

    Basic Template

---
+++ UsedCarProject/UsedCarProject.Entities/Stocks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UsedCarProject.Entities
+{
+    public class Stocks
+    {
+
+        
+        public int StockId { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public int Kilometers { get; set; }
+        [Required]
+        public int FuelTypeId { get; set; }
+        [Required]
+        public int CityId { get; set; }
+        [Required]
+        public int ColorId { get; set; }
+
+        public int FuelEconomy { get; set; }
+
+        [Required]
+        public int MakeId { get; set; }
+        [Required]
+        public int ModelId { get; set; }
+        [Required]
+        public int VersionId { get; set; }
+
+    }
+}
---
+++ UsedCarProject/UsedCarProject.Services/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UsedCarProject.Services.MvcApplication" Language="C#" %>
