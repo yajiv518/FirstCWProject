aaee9b81190a2548646497d2597306a4b6a2745f
Vijay Prajapati <vijay.prajapati@aepl.com>
2017-06-27 18:14:15 +0530

    DAL logic implemented

---
+++ UsedCars/DAL/DataAccessLayer.cs
@@ -0,0 +1,81 @@
+﻿using Entities;
+using Interfaces;
+using System;
+using Dapper;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace DAL
+{
+    public class DataAccessLayer:IDataAccess<Stocks>
+    {
+        private string connString = ConfigurationManager.ConnectionStrings["DatabaseConncet"].ConnectionString;
+
+        public int Create(Stocks stock)
+        {
+            int newId;
+            var param = new DynamicParameters();
+            param.Add("v_price", stock.Price, direction: ParameterDirection.Input);
+            param.Add("v_year", stock.Year, direction: ParameterDirection.Input);
+            param.Add("v_km", stock.Kilometers, direction: ParameterDirection.Input);
+            param.Add("v_fuelID", stock.FuelTypeId, direction: ParameterDirection.Input);
+            param.Add("v_cityID", stock.CityId, direction: ParameterDirection.Input);
+            param.Add("v_colorID", stock.ColorId, direction: ParameterDirection.Input);
+            param.Add("v_makeID", stock.MakeId, direction: ParameterDirection.Input);
+            param.Add("v_modelID", stock.ModelId, direction: ParameterDirection.Input);
+            param.Add("v_versionID", stock.VersionId, direction: ParameterDirection.Input);
+            if(stock.FuelEconomy!=null)
+                param.Add("v_fuelEco", stock.FuelEconomy, direction: ParameterDirection.Input);
+            param.Add("v_id", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            using(IDbConnection conn=new MySqlConnection(connString))
+            {
+                conn.Execute("sp_UsedCarsCreate",param,commandType:CommandType.StoredProcedure);
+            }
+            newId = param.Get<int>("v_id");
+            return newId;
+        }
+
+        public int Edit(int id,Stocks stock)
+        {
+            int tag=0;
+            var param = new DynamicParameters();
+            param.Add("v_price", stock.Price, direction: ParameterDirection.Input);
+            param.Add("v_year", stock.Year, direction: ParameterDirection.Input);
+            param.Add("v_km", stock.Kilometers, direction: ParameterDirection.Input);
+            param.Add("v_fuelID", stock.FuelTypeId, direction: ParameterDirection.Input);
+            param.Add("v_cityID", stock.CityId, direction: ParameterDirection.Input);
+            param.Add("v_colorID", stock.ColorId, direction: ParameterDirection.Input);
+            param.Add("v_makeID", stock.MakeId, direction: ParameterDirection.Input);
+            param.Add("v_modelID", stock.ModelId, direction: ParameterDirection.Input);
+            param.Add("v_versionID", stock.VersionId, direction: ParameterDirection.Input);
+            param.Add("v_id", id, direction: ParameterDirection.Input);
+            if (stock.FuelEconomy != null)
+                param.Add("v_fuelEco", stock.FuelEconomy, direction: ParameterDirection.Input);
+            param.Add("v_tag", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            using (IDbConnection conn = new MySqlConnection(connString))
+            {
+                conn.Execute("sp_UsedCarsEdit", param, commandType: CommandType.StoredProcedure);
+            }
+            tag = param.Get<int>("v_tag");
+            return tag;
+        }
+
+        public int Delete(int id)
+        {
+            int tag=0;
+            var param = new DynamicParameters();
+            param.Add("v_tag", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            using (IDbConnection conn = new MySqlConnection(connString))
+            {
+                conn.Execute("sp_UsedCarsEdit", param, commandType: CommandType.StoredProcedure);
+            }
+            tag = param.Get<int>("v_tag");
+            return tag;
+        }
+    }
+}
