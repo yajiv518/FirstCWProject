8382317f2726e81e8576a1c9ad56653f5ed92c6d
Palash Jain <palash.jain@aepl.com>
2017-06-28 10:52:40 +0530

    Cached Layer

---
+++ UsedCars/Cache/cacheLayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Enyim.Caching;
+using Enyim.Caching.Memcached;
+using DAL;
+
+namespace Cache
+{
+    public class cacheLayer
+    {
+
+        MemcachedClient client = new MemcachedClient();
+        public object getall(int carId)
+        {
+            string key = "UsedCar_" + Convert.ToString(carId);
+            object carDetails = client.Get(key);
+            if (carDetails != null)
+                return carDetails;
+            else
+            {
+                DataAccessLayer GetDetails = new DataAccessLayer();
+                object stockDetail=GetDetails.Read(carId);
+                 client.Store(StoreMode.Set, key, stockDetail,DateTime.Now.AddMinutes(15));
+                return stockDetail;
+
+            }
+        }
+    }
+}
