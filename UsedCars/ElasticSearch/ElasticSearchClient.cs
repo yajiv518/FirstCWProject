e7fac3507178e7a3c9b6240eac19d23d9771e071
Vijay Prajapati <vijay.prajapati@aepl.com>
2017-06-29 16:03:43 +0530

    ES delete and update added

--- UsedCars/ElasticSearch/ElasticSearchClient.cs
+++ UsedCars/ElasticSearch/ElasticSearchClient.cs
@@ -12,30 +12,30 @@ namespace ElasticSearch
     public class ElasticSearchClient
     {
         ElasticClient client = ElasticClientInstance.GetInstance();
-        public IEnumerable<ReadStock> GetAllStock()
+        public IEnumerable<ESGetDetail> GetAllStock()
         {
-            var searchResult = client.Search<ReadStock>(s => s
+            var searchResult = client.Search<ESGetDetail>(s => s
                                                         .Index("stockdata_g3")
                                                         .Type("esgetdetail")
                                                         .MatchAll());
-            IEnumerable<ReadStock> getAllStock=searchResult.Documents.ToArray<ReadStock>();
+            IEnumerable<ESGetDetail> getAllStock = searchResult.Documents.ToArray<ESGetDetail>();
             return getAllStock;
         }
-        public IEnumerable<ReadStock> GetStockByCity(string cityName)
+        public IEnumerable<ESGetDetail> GetStockByCity(string cityName)
         {
-            var searchResult = client.Search<ReadStock>(s => s
+            var searchResult = client.Search<ESGetDetail>(s => s
                                                         .Index("stockdata_g3")
                                                         .Type("esgetdetail")
                                                         .Query(q=>q
                                                             .Term(p=>p.City,cityName)
                                                             )
                                                         );
-            IEnumerable<ReadStock> getAllStock = searchResult.Documents.ToArray<ReadStock>();
+            IEnumerable<ESGetDetail> getAllStock = searchResult.Documents.ToArray<ESGetDetail>();
             return getAllStock;
         }
-        public IEnumerable<ReadStock> GetStockByBudget(int minValue,int maxValue)
+        public IEnumerable<ESGetDetail> GetStockByBudget(int minValue, int maxValue)
         {
-            var searchResult = client.Search<ReadStock>(s => s
+            var searchResult = client.Search<ESGetDetail>(s => s
                                                         .Index("stockdata_g3")
                                                         .Type("esgetdetail")
                                                         .From(1)
@@ -46,8 +46,34 @@ namespace ElasticSearch
                                                                 .GreaterOrEquals(minValue)
                                                                 )
                                                             ));
-            IEnumerable<ReadStock> getAllStock = searchResult.Documents.ToArray<ReadStock>();
+            IEnumerable<ESGetDetail> getAllStock = searchResult.Documents.ToArray<ESGetDetail>();
             return getAllStock;
         }
+        public void UpdateStock(int id, ESGetDetail updatedStock)
+        {
+            client.Update<ESGetDetail, object>(u => u
+                .Id(id)
+                .Doc(new { Price = updatedStock.Price,
+                           Year = updatedStock.Year,
+                           Kilometers = updatedStock.Kilometers,
+                           FuelType = updatedStock.FuelType,
+                           City = updatedStock.City,
+                           carcompany = updatedStock.carcompany,
+                           modelname = updatedStock.modelname,
+                           carversionname = updatedStock.carversionname
+                })
+                .RetryOnConflict(3)
+                .Refresh()
+            );
+        }
+        public void DeleteStock(int id)
+        {
+            client.DeleteByQuery<ESGetDetail>(q => q
+             .AllIndices()
+             .Query(rq => rq
+                 .Term(f => f.ID, id)
+                )
+            );
+        }	
     }
 }
